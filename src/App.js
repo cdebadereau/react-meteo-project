881d5f8af5fe0e7ab0f7cd7f06ad484fad7e7158
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2024-09-07 16:45:37 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by Cyrielle de Badereau de Saint Martin and is{" "}
-        <a
-          href="https://github.com/cdebadereau/react-meteo-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was coded by Cyrielle de Badereau de Saint Martin and is{" "}
+          <a
+            href="https://github.com/cdebadereau/react-meteo-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
